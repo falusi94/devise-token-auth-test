7bd015d893552d5201bc3e525d90638fd217de3c
David Falusi <falusi.david94@gmail.com>
2020-08-31 23:46:47 +0200

    Simplify models - remove unnecessary devise concerns

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,8 @@ class Employee < ApplicationRecord
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable
   include DeviseTokenAuth::Concerns::User
 end
